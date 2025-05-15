effe162948cebbb0f334704bcf54dac247e5c3e8
Kim-Hyunhee <tta_bi@naver.com>
2025-05-15 13:24:37 +0900

    [Refactor] 폴더 이름 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { JounalModule } from './session/jounal.module';
+import { JounalModule } from './jounal/jounal.module';
 
 @Module({
   imports: [JounalModule],
