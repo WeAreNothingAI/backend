66a8bf5c6b2834eaeb053495b1ce443978b66d37
박준규 <junju404@naver.com>
2025-05-22 11:33:38 +0900

    [Feat] helmet 보안 및 공통 response 처리를 위한 파이프 및 인터셉터  추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ResponseInterceptor } from './config/response/response.interceptor';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 설정
+  app.enableCors({
+    origin: ['http://localhost:3000'], // Next.js 기본 포트
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // 쿠키, 인증 헤더 등을 포함
+  });
+
+  // 보안 미들웨어
+  app.use(helmet()); // 기본적인 보안 헤더 설정
+
+  // 전역 파이프 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // DTO 클래스로 자동 변환
+      whitelist: true, // DTO에 정의되지 않은 속성 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 있으면 요청 자체를 막음
+    }),
+  );
+
+  // 전역 인터셉터 설정
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Oncare API')
