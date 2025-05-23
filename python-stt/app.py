4a0545e88149d7b614093b4a61d27ab00be1f36c
Kim-Hyunhee <tta_bi@naver.com>
2025-05-23 14:53:56 +0900

    [Build] python-stt 배포

--- python-stt/app.py
+++ python-stt/app.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
-import whisper
 import tempfile
 import os
 import uvicorn
