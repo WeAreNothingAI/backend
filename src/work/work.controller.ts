167d7eb682779b0878314ac7aa9d5ed1ba78194f
Kim-Hyunhee <tta_bi@naver.com>
2025-05-28 11:46:06 +0900

    [Feat] #13 근무 시작 API 구현

---
+++ src/work/work.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { WorkService } from './work.service';
+
+@ApiTags('work')
+@Controller('work')
+export class WorkController {
+  constructor(private workService: WorkService) {}
+
+  @Post('start/:id')
+  async postWorkIn(@Param('id', ParseIntPipe) memberId: number) {
+    return await this.workService.createWorkIn(memberId);
+  }
+}
