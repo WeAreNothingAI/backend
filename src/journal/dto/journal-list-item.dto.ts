78cb3a90085861f48c9dc6689f8b4222c80d4122
NanYoung Kim <zlfkcmspdbdp@naver.com>
2025-06-03 16:50:45 +0900

    [Refactor] DTO를 Date로 변경

--- src/journal/dto/journal-list-item.dto.ts
+++ src/journal/dto/journal-list-item.dto.ts
@@ -5,8 +5,5 @@ export class JournalListItemDto {
   id: number;
 
   @ApiProperty({ example: '2025-05-26T04:43:53.853Z', description: '생성일자' })
-  createdAt: string;
-
-  @ApiProperty({ example: '대상자님은 2025년 5월 26일에 상담사와 상담을 진행하고 계십니다.', description: '상담 요약' })
-  summary: string;
+  createdAt: Date;
 } 
