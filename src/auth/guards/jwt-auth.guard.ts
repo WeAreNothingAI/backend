9a796494cc3b0967de275f37e5ea9753f5b4ff83
Kim-Hyunhee <tta_bi@naver.com>
2025-06-04 11:44:51 +0900

    [Refactor] jwt-guard 커스터마이징
    - 토큰 만료 표시 하기

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err: any, user: any, info: any) {
+    if (info?.message === 'jwt expired') {
+      throw new UnauthorizedException('토큰이 만료되었습니다.');
+    }
+
+    if (err || !user) {
+      throw new UnauthorizedException('인증이 필요합니다.');
+    }
+
+    return user;
+  }
+}
